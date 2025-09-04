20c6596f9d758770e5025479067242c948296e0a
GiuliaAuri <314739@studenti.unimore.it>
2025-09-04 19:45:18 +0200

    fix bugs

--- actuators/irrigation_actuator.py
+++
@@ -1,9 +0,0 @@
-from model.SwitchActuator import SwitchActuator
-
-
-class IrrigationActuator(SwitchActuator):
-    def __init__(self, plant_id:str):
-        super().__init__(plant_id=plant_id, type="irrigation_actuator", device="irrigation_actuator")
-      
-
-
